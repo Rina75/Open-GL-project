2f563f8b5acd33ed200de927ddeaecfa365d989c
Rina75 <103808213+Rina75@users.noreply.github.com>
2022-09-28 08:28:16 +0800

    Update main.cpp

--- Garis/main.cpp
+++ Garis/main.cpp
@@ -5,14 +5,12 @@
 void display(void){
 glClear (GL_COLOR_BUFFER_BIT);
 
-    glPushMatrix ();
-    glClearColor(1,1,1,0);
     glColor3f(1.0,0.0,0.0);
 
     glBegin(GL_LINES);
 
-    glVertex3f(-7.0,5.0,2.0);
-    glVertex3f(0.0,0.0,0.0);
+    glVertex3f(-0.4,0.6,0.0);
+    glVertex3f(0.7,-0.2,0.0);
 
     glEnd ();
 glFlush();
